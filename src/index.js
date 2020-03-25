13d7a9339e50410435c1d2474703fa35e5896de4
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-03-25 16:01:50 +0100

    change token for git

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js');
 
+require('dotenv').config();
+
 const client = new Discord.Client();
 
 client.once('ready', () => {
   console.log('Ready!');
+});
+
+client.on('message', msg => {
+  console.log(msg);
 });
 
 client.login(process.env.BOT_TOKEN);

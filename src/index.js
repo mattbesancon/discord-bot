68887c3733d7df49c5f09a77d9df34a606df77e9
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-03-28 19:35:27 +0100

    adding commands

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
-
 require('dotenv').config();
+const commandHandler = require('./commands');
 
 const client = new Discord.Client();
 
@@ -8,8 +8,6 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.on('message', msg => {
-  console.log(msg);
-});
+client.on('message', commandHandler);
 
 client.login(process.env.BOT_TOKEN);
